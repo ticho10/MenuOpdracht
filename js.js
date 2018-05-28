63174272df889539ed5eb11c68864cadd43f1d07
Poffertjeskip <30628312+ticho10@users.noreply.github.com>
2018-05-28 21:19:22 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+// als op een link met een submenu geklikt wordt, moet ik niet naar de koppeling gaan
+
+let heeftSub = document.querySelectorAll('.heeft-submenu > a');
+
+console.log(heeftSub);
+
+for (let i=0; i<heeftSub.length; i++){
+    heeftSub[i].addEventListener('click', (e)=> e.preventDefault());
+}
